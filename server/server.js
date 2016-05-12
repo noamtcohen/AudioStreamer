6f591dc3b28242200a81c70176e87307a7081b25
alangumer <alangumer@gmail.com>
2016-05-12 16:50:36 -0400

    added MP3 lame encoder

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ var binaryServer = require('binaryjs').BinaryServer,
     fs = require('fs'),
     connect = require('connect'),
     serveStatic = require('serve-static'),
-    UAParser = require('./ua-parser');
+    UAParser = require('./ua-parser'),
+    lame = require('lame');
 
 var uaParser = new UAParser();
 
@@ -34,7 +35,8 @@ var server = binaryServer({server:server});
 server.on('connection', function(client) {
     console.log("new connection...");
     var fileWriter = null;
-
+    var writeStream = null;
+    
     var userAgent  =client._socket.upgradeReq.headers['user-agent'];
     uaParser.setUA(userAgent);
     var ua = uaParser.getResult();
@@ -42,19 +44,31 @@ server.on('connection', function(client) {
     client.on('stream', function(stream, meta) {
 
         console.log("Stream Start@" + meta.sampleRate +"Hz");
-        var fileName = "recordings/"+ ua.os.name +"-"+ ua.os.version +"_"+ new Date().getTime()  + ".wav"
-        fileWriter = new wav.FileWriter(fileName, {
+        var fileName = "recordings/"+ ua.os.name +"-"+ ua.os.version +"_"+ new Date().getTime();
+        
+        /* encode in WAV */
+        /*fileWriter = new wav.FileWriter(fileName + ".wav", {
             channels: 1,
             sampleRate: meta.sampleRate,
             bitDepth: 16
         });
-
-        stream.pipe(fileWriter);
+        stream.pipe(fileWriter);*/
+        
+        /* encode in MP3 */
+        writeStream = fs.createWriteStream( fileName + ".mp3" );
+        stream.pipe( new lame.Encoder(
+          {
+            channels: 1, bitDepth: 16, sampleRate: 44100, bitRate: 128, outSampleRate: 22050, mode: lame.MONO
+          })
+        )
+        .pipe( writeStream );
     });
 
     client.on('close', function() {
-        if (fileWriter != null) {
+        if ( fileWriter != null ) {
             fileWriter.end();
+        } else if ( writeStream != null ) {
+            writeStream.end();
         }
         console.log("Connection Closed");
     });
