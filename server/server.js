bb5c9d2a12cd98e400f77a7c120a1e1d6a4792b5
noamtcohen <noamtcohen@users.noreply.github.com>
2016-11-09 23:33:06 +0200

    if to switch

--- server/server.js
+++ server/server.js
@@ -47,23 +47,24 @@ server.on('connection', function(client) {
         console.log("Stream Start@" + meta.sampleRate +"Hz");
         var fileName = "recordings/"+ ua.os.name +"-"+ ua.os.version +"_"+ new Date().getTime();
         
-        if(CONFIG.AudioEnconding==="WAV"){
-            fileWriter = new wav.FileWriter(fileName + ".wav", {
+        switch(CONFIG.AudioEncoding){
+            case "WAV":
+                fileWriter = new wav.FileWriter(fileName + ".wav", {
                 channels: 1,
                 sampleRate: meta.sampleRate,
-                bitDepth: 16
-            });
-            stream.pipe(fileWriter);
-        }
-        else if(CONFIG.AudioEncoding==="MP3"){
-            writeStream = fs.createWriteStream( fileName + ".mp3" );
-            stream.pipe( new lame.Encoder(
-              {
-                channels: 1, bitDepth: 16, sampleRate: 44100, bitRate: 128, outSampleRate: 22050, mode: lame.MONO
-              })
-            )
-            .pipe( writeStream );
-        }
+                bitDepth: 16 });
+                stream.pipe(fileWriter);
+                break;
+                
+            case "MP3":
+                writeStream = fs.createWriteStream( fileName + ".mp3" );
+                stream.pipe( new lame.Encoder(
+                  {
+                    channels: 1, bitDepth: 16, sampleRate: 44100, bitRate: 128, outSampleRate: 22050, mode: lame.MONO
+                  })
+                )
+                .pipe( writeStream );
+                break;
     });
 
     client.on('close', function() {
